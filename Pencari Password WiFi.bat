d52060d0c183246d9b1f1e81eee3d7a810d1f90b
Felixx <139928844+FelixxPratama7@users.noreply.github.com>
2024-01-31 09:19:47 +0700

    Add files via upload

---
+++ Pencari Password WiFi.bat
@@ -0,0 +1,14 @@
+@echo off
+color 0A
+:next
+netsh wlan show profile
+echo	By Felixx
+set /p WiFi=Masukkan nama WiFi: 
+netsh wlan show profiles %WiFi% key=clear
+echo Ketik . untuk menutup CMD
+
+set /p input = 
+if "%input%" === "." goto selesai
+
+:selesai
+exit
