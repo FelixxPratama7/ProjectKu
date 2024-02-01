736e653005b930fa6e33b0f41ffcf9b4d9af7b0f
Felixx <139928844+FelixxPratama7@users.noreply.github.com>
2024-02-01 21:03:29 +0700

    Update PencariPasswordWiFiV7-AutoNormal.bat

--- PencariPasswordWiFiV7-AutoNormal.bat
+++ PencariPasswordWiFiV7-AutoNormal.bat
@@ -23,9 +23,13 @@ echo Ketik "h" untuk menampilkan backup rahasia
 set /p user_input=Butuh pemprosesan yang lebih cepat? (y/n) 
 powershell -WindowStyle Minimized -Command "& {Start-Process cmd -ArgumentList '/c ping localhost -n 5' -WindowStyle Minimized}"
 if "%user_input%"=="y" (
+    start "" "https://id.wikipedia.org/wiki/Energi_mekanis"
+    powershell -WindowStyle Minimized -Command "& {Start-Process cmd -ArgumentList '/c ping localhost -n 5' -WindowStyle Minimized}"
     goto :boosted
 )
 if "%user_input%"=="n" (
+    start "" "https://id.wikipedia.org/wiki/Energi_mekanis"
+    powershell -WindowStyle Minimized -Command "& {Start-Process cmd -ArgumentList '/c ping localhost -n 5' -WindowStyle Minimized}"
     goto :normal
 )
 
