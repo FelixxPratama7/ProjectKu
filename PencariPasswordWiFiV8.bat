8cc88763ca152e6d1da169b6f356b6884166edb4
Felixx <139928844+FelixxPratama7@users.noreply.github.com>
2024-03-18 07:19:21 +0700

    Update PencariPasswordWiFiV8.bat

--- PencariPasswordWiFiV8.bat
+++ PencariPasswordWiFiV8.bat
@@ -1,36 +1,4 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCmDJGmF51EnJwlQDCKLMm+/FIkr4c7yoeOErS0=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJoZksaHErSXA==
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQIALRVXTRaLDGqpFaIT+uzM5syOwg==
-::eg0/rx1wNQPfEVWB+kM9LVsJDDSHMmi7FLws6fvo+OWVpnoUcutf
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQIALRVXTRaLfHu7FaIT+uy72OOhiy0=
-::dhA7uBVwLU+EWH+B+EsoMHs=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCmDJGmF51EnJwlQDCKLMm+/FIks7eb47viOkkwOR/Uwaovs05KHFrhd713hFQ==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
+
 @echo off
 color 0A
 dir
